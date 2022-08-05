598e4f1b771125109cad837d5708325213b51b4a
Mario Lisbona <mario.lisbona@gmail.com>
2022-08-05 17:03:08 +1000

    create basic logic to show and hide dropdown meny

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-// const companyIcon = document.querySelector('.company-icon')
-// const featuresIcon = document.querySelector('.features-icon')
+const companyIcon = document.querySelector('.company-icon')
+const companyDropDown = document.querySelector('.company')
+const featuresIcon = document.querySelector('.features-icon')
+const featuresDropDown = document.querySelector('.features')
 
-// console.log(companyIcon)
-// console.log(featuresIcon)
 
-// companyIcon.classList.remove('fa-angle-down')
-// companyIcon.classList.add('fa-angle-up')
+
+companyIcon.addEventListener('click', function(e) {
+    swapArrow(e);
+
+    if (!companyDropDown.matches('.hide-dropdown')) {
+        companyDropDown.classList.add('hide-dropdown')
+    } else if (companyDropDown.matches('.hide-dropdown')) {
+        companyDropDown.classList.remove('hide-dropdown')
+    }
+})
+
+featuresIcon.addEventListener('click', function(e) {
+    swapArrow(e);
+
+    if (!featuresDropDown.matches('.hide-dropdown')) {
+        featuresDropDown.classList.add('hide-dropdown')
+    } else if (featuresDropDown.matches('.hide-dropdown')) {
+        featuresDropDown.classList.remove('hide-dropdown')
+    }
+})
+
+function swapArrow(event) {
+    if (event.target.matches('.fa-angle-down')) {
+        event.target.classList.remove('fa-angle-down');
+        event.target.classList.add('fa-angle-up');
+    } else if (!event.target.matches('.fa-angle-down')) {
+        event.target.classList.add('fa-angle-down');
+        event.target.classList.remove('fa-angle-up');
+    }
+}
