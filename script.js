2cb8220effe919bdfc1e566193b4c4748e33f7e3
Mario Lisbona <mario.lisbona@gmail.com>
2022-08-05 14:53:11 +1000

    comment out javascript

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const companyIcon = document.querySelector('.company-icon')
-const featuresIcon = document.querySelector('.features-icon')
+// const companyIcon = document.querySelector('.company-icon')
+// const featuresIcon = document.querySelector('.features-icon')
 
-console.log(companyIcon)
-console.log(featuresIcon)
+// console.log(companyIcon)
+// console.log(featuresIcon)
 
-companyIcon.classList.remove('fa-angle-down')
-companyIcon.classList.add('fa-angle-up')
+// companyIcon.classList.remove('fa-angle-down')
+// companyIcon.classList.add('fa-angle-up')
