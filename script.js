1e7634fdace120c0305850fdeb807f14ed05bb42
Mario Lisbona <mario.lisbona@gmail.com>
2022-08-05 21:38:58 +1000

    create function for dropdownaction and popupaction

--- script.js
+++ script.js
@@ -1,36 +1,70 @@
+const companyBtn = document.querySelector('.btn-dropdown-company')
 const companyIcon = document.querySelector('.company-icon')
 const companyDropDown = document.querySelector('.company')
+const featuresBtn = document.querySelector('.btn-dropdown-features')
 const featuresIcon = document.querySelector('.features-icon')
 const featuresDropDown = document.querySelector('.features')
 
+// companyBtn.addEventListener('click', function(e) {
 
+//     dropDownAction(e ,companyDropDown)
+// })
+
+// featuresBtn.addEventListener('click', function(e) {
+
+//     dropDownAction(e ,featuresDropDown)
+// })
 
 companyIcon.addEventListener('click', function(e) {
-    swapArrow(e);
 
-    if (!companyDropDown.matches('.hide-dropdown')) {
-        companyDropDown.classList.add('hide-dropdown')
-    } else if (companyDropDown.matches('.hide-dropdown')) {
-        companyDropDown.classList.remove('hide-dropdown')
-    }
+    dropDownAction(e ,companyDropDown)
 })
 
 featuresIcon.addEventListener('click', function(e) {
-    swapArrow(e);
 
-    if (!featuresDropDown.matches('.hide-dropdown')) {
-        featuresDropDown.classList.add('hide-dropdown')
-    } else if (featuresDropDown.matches('.hide-dropdown')) {
-        featuresDropDown.classList.remove('hide-dropdown')
-    }
+    dropDownAction(e ,featuresDropDown)
 })
 
-function swapArrow(event) {
+
+window.onclick = function(e) {
+
+    popUpAction(e, companyDropDown, companyIcon, '.company-icon')
+    popUpAction(e, featuresDropDown, featuresIcon, '.features-icon')
+
+}
+
+
+
+
+function dropDownAction(event, menu) {
+
     if (event.target.matches('.fa-angle-down')) {
         event.target.classList.remove('fa-angle-down');
         event.target.classList.add('fa-angle-up');
     } else if (!event.target.matches('.fa-angle-down')) {
         event.target.classList.add('fa-angle-down');
         event.target.classList.remove('fa-angle-up');
+    }
+
+    if (!menu.matches('.hide-dropdown')) {
+        menu.classList.add('hide-dropdown')
+    } else if (menu.matches('.hide-dropdown')) {
+        menu.classList.remove('hide-dropdown')
+        menu.classList.add('show-dropdown')
+    }
+}
+
+function popUpAction(event, dropdown, icon, className) {
+    if (!event.target.matches(className)) {
+
+        if (dropdown.classList.contains('show-dropdown')) {
+            dropdown.classList.remove('show-dropdown')
+            dropdown.classList.add('hide-dropdown')
+        }
+
+        if (icon.classList.contains('fa-angle-up')) {
+            icon.classList.remove('fa-angle-up')
+            icon.classList.add('fa-angle-down')
+        }
     }
 }
